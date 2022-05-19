bce5a3d8772282d9bc55ed59201786da82ca29a7
Parv <parv3213@gmail.com>
2022-05-19 15:33:24 +0530

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const avgPrice = require('./usdtTicket')
 const bodyParser = require('body-parser')
+let cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 
 app.get('/', async (req, res) => {
   const price = await avgPrice()
